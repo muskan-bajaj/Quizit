c88efe9228eba589f313ff014cee671d4c5645e0
muskan-bajaj <muskanvijaybajaj@gmail.com>
2024-11-20 14:22:27 +0530

    msg

--- Frontend/src/components/SubmissionAnalysis.jsx
+++ Frontend/src/components/SubmissionAnalysis.jsx
@@ -17,7 +17,6 @@ export default function SubmissionAnalysis() {
       const response = await axios.get(
         `http://localhost:3000/test/details?tid=${id}`
       );
-      console.log(response);
       setData(response.data);
       setQuestionBank(response.data.questionBanks);
       console.log(questionBank);
@@ -40,32 +39,91 @@ export default function SubmissionAnalysis() {
       />
       <div className={css.submissionScreen}>
         <div className={css.heading}>Question {current + 1}</div>
-        {questionBank[current] && (
-          <div className={css.inputs}>
-            <label htmlFor="question">Question</label>
-            <input
-              id="question"
-              type="text"
-              value={questionBank[current].question}
-              disabled
-            />
+        <div className={css.answerScreen}>
+          {data && data.instructions ? (
+            <div className={css.instructions}>
+              Instruction:
+              <br />
+              {data.instructions}
+            </div>
+          ) : (
+            <></>
+          )}
+          <div className={css.question}>
+            Question:
+            <br />
+            {data && data.questionBanks[current].question}
           </div>
-        )}
-        {questionBank[current] && (
-          <div className={css.inputs}>
-            <label htmlFor="answer">Expected Answer</label>
-            <input
-              id="answer"
-              type="text"
-              value={
-                questionBank[current].type == "choice"
-                  ? questionBank[current].answer?.join(", ") || ""
-                  : questionBank[current].answer
-              }
-              disabled
-            />
+          {data && data.questionBanks[current].type == "long" ? (
+            <div className={css.long}>
+              <textarea
+                // value={answer.length > 0 ? answer[0] : ""}
+                // onChange={(e) => {
+                //   setAnswer([e.target.value]);
+                // }}
+                placeholder="Enter Your Answer..."
+                rows={15}
+              />
+            </div>
+          ) : (
+            <div className={css.choice}>
+              {data &&
+                data.questionBanks[current].options.map((value, index) => {
+                  return (
+                    <label key={index} className={css.checkbox}>
+                      <input
+                        type="checkbox"
+                        value={value.option}
+                        // checked={answer.includes(value.option)}
+                        // onChange={(e) => {
+                        //   if (e.target.checked) {
+                        //     setAnswer([...answer, e.target.value]);
+                        //   } else {
+                        //     setAnswer(
+                        //       answer.filter(
+                        //         (item) => item !== e.target.value
+                        //       )
+                        //     );
+                        //   }
+                        // }}
+                      />
+                      <span className={css.checkmark}></span>
+                      {value.option}
+                    </label>
+                  );
+                })}
+            </div>
+          )}
+          <div className={css.nextButton}>
+            {/* {data && visited.length == data.questionCount ? (
+                <button
+                  onClick={() => {
+                    setViewSubmitModal(true);
+                    submitAnswerHandler(
+                      data.questionBanks[visited.length - 1].qid,
+                      answer
+                    );
+                  }}
+                >
+                  Submit Test
+                </button>
+              ) : ( */}
+            <button
+              onClick={() => {
+                // submitAnswerHandler(
+                //   data.questionBanks[visited.length - 1].qid,
+                //   answer
+                // );
+                // setVisited([...visited, true]);
+                // setAnswer([]);
+                setCurrent(current + 1);
+              }}
+            >
+              Next Question
+            </button>
+            {/* )} */}
           </div>
-        )}
+        </div>
       </div>
     </div>
   );
