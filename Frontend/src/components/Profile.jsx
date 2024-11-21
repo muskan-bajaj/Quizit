f5f5cb5de0e1938e6bf7be51e9847f4ef5f4ce3d
muskan-bajaj <muskanvijaybajaj@gmail.com>
2024-11-21 12:43:24 +0530

    profile updated

--- Frontend/src/components/Profile.jsx
+++ Frontend/src/components/Profile.jsx
@@ -1,11 +1,30 @@
-// import React from "react";
+import { useContext } from "react";
+
 import SideBar from "./SideBar";
+import AuthContext from "../store/AuthContext";
+
+import css from "../css/Profile.module.css";
 
 export default function Profile() {
+  const authCtx = useContext(AuthContext);
+
   return (
     <div className="flexpage">
       <SideBar />
-      <div></div>
+      <div className={css.ProfileBox}>
+        <div className={css.heading}>Welcome {authCtx.user.access}</div>
+        <br />
+        <div className={css.details}>
+          {authCtx.user.access} Details:
+          <br />
+          <br />
+          &nbsp;&nbsp;&nbsp;<span>Email: {authCtx.user.email}</span>
+          <br />
+          &nbsp;&nbsp;&nbsp;<span>Roll No: {authCtx.user.rollNo}</span>
+          <br />
+          &nbsp;&nbsp;&nbsp;<span>Name: {authCtx.user.name}</span>
+        </div>
+      </div>
     </div>
   );
 }
